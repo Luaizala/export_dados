88d16ae7f0e4f35957be4cdcd961ba503036404f
Luana Luiza <155683986+Luaizala@users.noreply.github.com>
2025-11-15 01:34:56 -0300

    export_dado.py

--- export_dado.py.py
+++ export_dado.py.py
@@ -3,13 +3,14 @@ import os
 
 # define o nome do arquivo final
 arquivo = "registro_itens.xlsx"
-produtos = []
+produtos = [] # lista onde ficarão guardados todos os produtos
 
 def formatar_moeda(valor):
     return f"R$ {valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
 
 while True:
-    print("\n==== MENU ====")
+    # menu principal
+    print("\n==== MENU ====") 
     print("1 - Adicionar novo produto")
     print("2 - Sair e gerar Excel")
 
@@ -18,32 +19,34 @@ while True:
     if opcao == "1":
         nome_produto = input("Digite o nome do produto: ")
 
-        # Valida quantidade
+        # Valida quantidade (somente número inteiro)
         while True:
             try:
                 quantidade = input("Digite a quantidade comprada: ")
-                quantidade_validada = int(quantidade)
+                quantidade_validada = int(quantidade) # tenta converter para inteiro 
                 break
             except ValueError:
                 print("Por favor, insira um número válido para a quantidade.")
 
-        # Valida preço
+        # Valida preço (somente decimal)
         while True:
             try:
                 preco = input("Digite o preço unitário do produto: ")
-                preco_validado = float(preco)
+                preco_validado = float(preco) # tenta converter para float
                 break
             except ValueError:
                 print("Por favor, insira um número válido para o preço.")
 
         valor_total = quantidade_validada * preco_validado
+        # mostra o produto cadastrado
 
         print("\nProduto cadastrado:")
         print("Nome:", nome_produto)
         print("Quantidade:", quantidade_validada)
         print("Preço unitário:", formatar_moeda(preco_validado))
         print("Valor total:", formatar_moeda(valor_total))
-
+        
+# salva produto em formato de dicionário dentro da lista
         produtos.append({
             "Item": nome_produto,
             "Quantidade": quantidade_validada,
@@ -58,15 +61,16 @@ while True:
     else:
         print("Opção inválida. Tente novamente.")
 
-# === GERAÇÃO DO EXCEL ===
 
-df_novos = pd.DataFrame(produtos)
 
+df_novos = pd.DataFrame(produtos) # cria uma tabela e guarda na memória do python
+# se o arquivo não existir cria um novo
 if not os.path.exists(arquivo):
-    df_novos.to_excel(arquivo, index=False)
+    df_novos.to_excel(arquivo, index=False) # remove a coluna de índice (0, 1, 2...) excel
 else:
+    # se o arquivo existir, adiciona novos itens 
     df_existente = pd.read_excel(arquivo)
     df_atualizado = pd.concat([df_existente, df_novos], ignore_index=True)
-    df_atualizado.to_excel(arquivo, index=False)
+    df_atualizado.to_excel(arquivo, index=False) 
 
 print("Arquivo 'registro_itens.xlsx' atualizado com sucesso!")
